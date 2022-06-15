8a217ad6382100540024f1ef6ffad38d71e1db74
d-shweta <105386129+d-shweta@users.noreply.github.com>
2022-06-15 18:26:03 +0530

    Update ColorInfo.c

--- ColorInfo.c
+++ ColorInfo.c
@@ -1,3 +1,9 @@
+void ColorPairToString(const ColorPair* colorPair, char* buffer) {
+    sprintf(buffer, "%s %s",
+        MajorColorNames[colorPair->majorColor],
+        MinorColorNames[colorPair->minorColor]);
+}
+
 ColorPair GetColorFromPairNumber(int pairNumber) {
     ColorPair colorPair;
     int zeroBasedPairNumber = pairNumber - 1;
