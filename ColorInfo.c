ba4c80a27ba46622077988fc3ff143f9dfa04362
d-shweta <105386129+d-shweta@users.noreply.github.com>
2022-06-15 19:17:24 +0530

    Update ColorInfo.c

--- ColorInfo.c
+++ ColorInfo.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <assert.h>
 #include "main.h"
+
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+int numberOfMajorColors =
+    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+int numberOfMinorColors =
+    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
         MajorColorNames[colorPair->majorColor],
