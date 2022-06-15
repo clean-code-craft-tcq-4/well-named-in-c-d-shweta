adee9a744002892e842543f5d5c388257be5a16c
d-shweta <105386129+d-shweta@users.noreply.github.com>
2022-06-15 18:52:00 +0530

    Update main.h

--- main.h
+++ main.h
@@ -22,4 +22,5 @@ ColorPair GetColorFromPairNumber(int pairNumber);
 int GetPairNumberFromColor(const ColorPair* colorPair) ;
 void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);
 void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber);
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
 
