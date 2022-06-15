2f3c81e1a305d32ead529ff52ac1b1844b7a8b16
d-shweta <105386129+d-shweta@users.noreply.github.com>
2022-06-15 19:19:30 +0530

    Update PairInfo.c

--- PairInfo.c
+++ PairInfo.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <assert.h>
 #include "main.h"
+
+extern int numberOfMajorColors;
+
 int GetPairNumberFromColor(const ColorPair* colorPair) {
     return colorPair->majorColor * numberOfMinorColors +
             colorPair->minorColor + 1;
